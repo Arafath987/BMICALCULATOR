c2a1ed64c2094bb9a75879c5aab3272c9e2e21f3
Arafath987 <94624401+Arafath987@users.noreply.github.com>
2023-02-03 21:42:55 +0530

    bmi calculator added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [bmi, setbmi] = useState(null);
+  const [height, setheight] = useState(0);
+  const [weight, setweight] = useState(0);
+
+  function addbmi() {
+    setbmi(weight / height);
+    console.log(bmi);
+  }
+  function handleWeight(e) {
+    setweight(e.target.value);
+  }
+
+  function handleHeight(e) {
+    setheight(e.target.value);
+    console.log(height);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="outerbox">
+      <h1 className="h1">BMI CALCULATOR</h1>
+      <img
+        className="image"
+        src="https://images.pexels.com/photos/1092730/pexels-photo-1092730.jpeg?cs=srgb&dl=pexels-jane-doan-1092730.jpg&fm=jpg"
+      ></img>
+      <div className="innerbox">
+        <input
+          type="number"
+          className="log"
+          onChange={handleWeight}
+          placeholder="weight"
+        />
+        <br />
+        <input
+          type="number"
+          className="log"
+          onChange={handleHeight}
+          placeholder="height"
+        />
+        <br />
+        <button onClick={addbmi}>bmi calculate</button>
+        <p>bmi={bmi}</p>
+      </div>
     </div>
   );
 }
